84630f20d4b8a22c62b1e67dcd4632b14bc31d04
abhishekbhartiind <imabhishekbharti12@gmail.com>
2023-02-18 12:24:06 +0530

    added crud operations

---
+++ index.ts
@@ -0,0 +1,58 @@
+import express, { Request, Response, NextFunction } from "express"
+import { PrismaClient } from "@prisma/client"
+
+const app = express()
+app.use(express.json())
+
+const prisma = new PrismaClient()
+
+// POST
+app.post("/", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { username, password } = req.body
+    const user = await prisma.user.create({
+      data: {
+        username,
+        password,
+      },
+    })
+
+    res.json(user)
+  } catch (error) {
+    next(error)
+  }
+})
+
+// GET
+app.get("/", async (req: Request, res: Response) => {
+  const users = await prisma.user.findMany()
+  res.json(users)
+})
+
+// PUT
+app.put("/", async (req: Request, res: Response) => {
+  const { id, username } = req.body
+  const updateUser = await prisma.user.update({
+    where: {
+      id,
+    },
+    data: {
+      username,
+    },
+  })
+
+  res.json(updateUser)
+})
+
+app.delete("/:id", async (req: Request, res: Response) => {
+  const { id } = req.params
+  const deleteUser = await prisma.user.delete({
+    where: { id: Number(id) },
+  })
+
+  res.json(deleteUser)
+})
+
+app.listen(4000, () => {
+  console.log("Server is running on PORT: 4000")
+})
